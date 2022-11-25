8548134a4e18ee2e248c19af0baed2e615feae29
dalmasmaka <smakadalma@gmail.com>
2022-11-25 01:08:33 +0100

    Database Updated

--- ManagementSystem/ManagementSystem/Entities/Client.cs
+++ ManagementSystem/ManagementSystem/Entities/Client.cs
@@ -9,19 +9,42 @@ namespace ManagementSystem.Entities
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int ClientsId { get; set; }
-        [Required]
-        public string ClientsName { get; set; }
-        [Required]
-        public string ClientsSurname { get; set; }
-        public string ClientsPhoneNumber { get; set; }
-        public string ClientsCity { get; set; }
-        public string ClientsAddres { get; set; }
+        public int ClientId { get; set; }
+        [Required(ErrorMessage = "Name ist erforderlich")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Nachname ist erforderlich")]
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "Geschlecht ist erforderlich")]
+        public bool Gender { get; set; }
+        [Required(ErrorMessage = "Personenstand ist erforderlich")]
+        public string CivilStatus { get; set; }
+        [Required(ErrorMessage = "Stadt ist erforderlich")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "Adresse ist erforderlich")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Hausnummer ist erforderlich")]
         public int HouseNumber { get; set; }
+        [Required(ErrorMessage = "Türnummer ist erforderlich")]
         public int DoorNumber { get; set; }
-        public int StairNumber { get; set; }
-        public int? WorkerId { get; set; }
+        [Required(ErrorMessage = "Stiegenummer ist erforderlich")]
+        public int StairsNumber { get; set; }
+        [Required(ErrorMessage = "Postleitzahl ist erforderlich")]
+        public string Postal { get; set; }
+        [Required(ErrorMessage = "Email ist erforderlich")]
+        public string Email { get; set; }
+        public bool Status { get; set; }
+        [Required(ErrorMessage = "Bankkonto ist erforderlich")]
+        public string Bank { get; set; }
+
+        [Required(ErrorMessage = "IBAN ist erforderlich")]
+        public string IBAN { get; set; }
+        [Required(ErrorMessage = "Bic ist erforderlich")]
+        public string SwiftCode { get; set; }
+        public string Disease { get; set; }
+
+
         public int? AdvocateId { get; set; }
+        public int? WorkerId { get; set; }
         public virtual Worker Worker { get; set; }
         public virtual Advocate Advocate { get; set; }
 
--- ManagementSystem/ManagementSystem/Entities/Worker.cs
+++ ManagementSystem/ManagementSystem/Entities/Worker.cs
@@ -8,15 +8,57 @@ namespace ManagementSystem.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int WorkerId { get; set; }
-        [Required]
-        public string WorkersName { get; set; }
-        [Required]
-        public string WorkersSurnname { get; set; }
-        public string WorkersPhoneNumber { get; set; }
-        public string WorkersEmail { get; set; }
-        public string WorkersCity { get; set; }
-        public string WorkersAddress { get; set; }
+        [Required(ErrorMessage = "Name ist erforderlich")]
+        public string Name { get; set; }
 
-        
+        [Required(ErrorMessage = "Nachname ist erforderlich")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email ist erforderlich")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Stadt ist erforderlich")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Geschlecht ist erforderlich")]
+        public bool Gender { get; set; }
+
+        [Required(ErrorMessage = "Personenstand ist erforderlich")]
+        public string CivilStatus { get; set; }
+
+        [Required(ErrorMessage = "Ort ist erforderlich")]
+        public string Location { get; set; }
+
+        [Required(ErrorMessage = "Adresse ist erforderlich")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Strasse ist erforderlich")]
+        public string Street { get; set; }
+
+        [Required(ErrorMessage = "Postleitzahl ist erforderlich")]
+        public string Postal { get; set; }
+
+        [Required(ErrorMessage = "Hausnummer ist erforderlich")]
+        public string HouseNumber { get; set; }
+
+        [Required(ErrorMessage = "Stiegenummer ist erforderlich")]
+        public string StairsNumber { get; set; }
+
+        [Required(ErrorMessage = "Türnummer ist erforderlich")]
+        public string DoorNumber { get; set; }
+
+        [Required(ErrorMessage = "Bankkonto ist erforderlich")]
+        public string Bank { get; set; }
+
+        [Required(ErrorMessage = "IBAN ist erforderlich")]
+        public string IBAN { get; set; }
+
+        [Required(ErrorMessage = "Bic ist erforderlich")]
+        public string SwiftCode { get; set; }
+
+        [Required(ErrorMessage = "Salaris ist erforderlich")]
+        public long Salary { get; set; }
+
+
     }
 }
