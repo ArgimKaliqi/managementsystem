bb86d6e8623e3ddac94f8a91e01123d0cfc4ae47
EltonTahiri <eltontahiri4@gmail.com>
2022-11-25 01:45:31 +0100

    Created all the files and setup code
    Started implementing Code

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import Navbar from './components/Navbar/Navbar';
 import { Routes, Route } from 'react-router-dom'
 import LoginForm from './pages/Login/Login';
+import Board from './board';
 
 function App() {
   return (
     <>
-      <Routes>
+    <Routes>
         <Route path='/' element={<LoginForm />} />
         <Route path='/clients' element={<Navbar />} />
-        </Routes>    
-  </>
+        <Route path='/admin' element={<Board />} />
+        </Routes>   
+    </>
   );
 }
 
---
+++ client/src/board.js
@@ -0,0 +1,24 @@
+import { ColorModeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+
+function Board() {
+  const [theme, colorMode] =useMode()
+
+  return (
+    <>
+  <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    <div className='app'> 
+    <main className="content">
+      
+    </main>
+  </div>
+  </ThemeProvider>
+  </ColorModeContext.Provider>
+    
+    </>
+  );
+}
+
+export default Board;
