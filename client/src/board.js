66d3dacd4c94b5d55a9b72f7d35d50a211eb72c8
EltonTahiri <eltontahiri4@gmail.com>
2022-11-25 18:18:24 +0100

    Updates

--- client/src/board.js
+++ client/src/board.js
@@ -1,5 +1,19 @@
 import { ColorModeContext, useMode } from './theme'
 import { CssBaseline, ThemeProvider } from '@mui/material'
+import { Routes, Route} from 'react-router-dom'
+import Topbar from './pages/Dashboard/global/Topbar'
+import Sidebar from './pages/Dashboard/global/Sidebar'
+import Dashboard from './pages/Dashboard'
+// import Tasks from './pages/tasks'
+// import Clients from './pages/clients'
+// import Advocate from './pages/advocates'
+// import Coworkers from './pages/coworkers'
+// import Companies from './pages/companies'
+// import Reports from './pages/reports'
+// import Schedule from './pages/schedule'
+// import Borxhet from './pages/borxhet'
+// import Paushall from './pages/paushall'
+// import Line from './pages/Line'
 
 function Board() {
   const [theme, colorMode] =useMode()
@@ -10,8 +24,21 @@ function Board() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
     <div className='app'> 
-    <main className="content">
-      
+    <Sidebar />
+    <main className="content"><Topbar />
+      <Routes>
+          <Route path='/' element={<Dashboard />} />
+          {/* <Route path='/tasks' element={<Tasks />} /> */}
+          {/* <Route path='/clients' element={<Clients/>} /> */}
+          {/* <Route path='/advocate' element={<Advocate />} /> */}
+          {/* <Route path='/coworkers' element={<Coworkers/>} /> */}
+          {/* <Route path='/companies' element={<Companies/>} /> */}
+          {/* <Route path='/reports' element={<Reports/>} /> */}
+          {/* <Route path='/schedule' element={<Schedule/>} /> */}
+          {/* <Route path='/borxhet' element={<Borxhet/>} /> */}
+          {/* <Route path='/paushall' element={<Paushall/>} /> */}
+          {/* <Route path='/Line' element={<Line/>} /> */}
+      </Routes>
     </main>
   </div>
   </ThemeProvider>
--- client/src/pages/Dashboard/global/Sidebar.js
+++ client/src/pages/Dashboard/global/Sidebar.js
@@ -1,3 +1,11 @@
+import { useState } from 'react'
+import { ProSideBar, Menu, MenuItem } from 'react-pro-sidebar'
+import 'react-pro-sidebar/dist/css/styles.css'
+import { Box, IconButton, Typography, useTheme } from '@mui/material'
+import { Link } from "react-router-dom"
+import { tokens } from '../../../theme'
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined'
+
 
 const Sidebar = () => {
     return <div>Sidebar</div> 
