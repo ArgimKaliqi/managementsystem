9a2beff638bd83beff24d56dcace8302b04a1be8
dalmasmaka <smakadalma@gmail.com>
2022-12-19 20:11:18 +0100

    first commit for tasks

---
+++ ManagementSystem/ManagementSystem/DTOs/TasksDTO.cs
@@ -0,0 +1,21 @@
+﻿using ManagementSystem.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManagementSystem.DTOs
+{
+    public class CreateTasks
+    {
+        public int TaskID { get; set; }
+        public string TaskName { get; set; }
+        public string Description { get; set; }
+        public string DueDate { get; set; }
+    }
+    public class UodateTaskDTO
+    {
+        public int TaskID { get; set; }
+        public string TaskName { get; set; }
+        public string Description { get; set; }
+        public string DueDate { get; set; }
+    }
+}
---
+++ ManagementSystem/ManagementSystem/Entities/Tasks.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ManagementSystem.Entities
+{
+    public class Tasks
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TaskID { get; set; }
+        [Required]
+        public string TaskName { get; set; }
+        [Required]
+        public int? WorkerId { get; set; }
+        [Required]
+        public int? ClientId { get; set; }
+        public string Description { get; set; }
+        public string DueDate { get; set; }
+        public virtual Worker Worker { get; set; }
+        public virtual Client Client { get; set; }
+    }
+}
